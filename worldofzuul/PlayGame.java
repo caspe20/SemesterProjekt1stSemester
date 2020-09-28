d668db663c02af57c5f278f9e96506f88b0a9d0a
Speered <caspertaccopiv@gmail.com>
2020-09-28 20:54:56 +0200

    Initial Commit

---
+++ worldofzuul/PlayGame.java
@@ -0,0 +1,14 @@
+package worldofzuul;
+
+/**
+ * PlayGame This is the function for the chronological order of the game. Due to
+ * it being the start of the game, it also has the only main method that is
+ * present in the game.
+ */
+public class PlayGame {
+
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.play();
+    }
+}
