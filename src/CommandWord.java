746b34ee4be6c8a55bd33271d7b59e514480bf61
Simon <simok20@student.sdu.dk>
2020-11-03 09:19:16 +0100

    Initial commit

---
+++ src/CommandWord.java
@@ -0,0 +1,15 @@
+package PackageElements;
+
+public enum CommandWord {
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+
+    private String commandString;
+
+    CommandWord(String commandString) {
+        this.commandString = commandString;
+    }
+
+    public String toString() {
+        return commandString;
+    }
+}
