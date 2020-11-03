fe1266cf1f1296daa825b3eee9789440db9011b6
Speered <caspertaccopiv@gmail.com>
2020-11-03 10:22:05 +0100

    Created basic class
    Created class' based on UML for implementation

---
+++ src/UpgradePath.java
@@ -0,0 +1,17 @@
+public class UpgradePath {
+    public Upgrade[] upgrades;
+    public String pathName;
+    public int currentLevel;
+    public int currentProduction;
+
+    public void upgrade(){
+
+    }
+
+    public double getUpgradePrice(){
+        return 0;
+    }
+    public String getNameOfNextUpgrade(){
+
+    }
+}
