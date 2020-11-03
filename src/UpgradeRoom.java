fc77aa194f9e5b6e3a85072de96477ceb1d34433
Pernille Stoltze Hansen <pernh20@student.sdu.dk>
2020-11-03 12:24:34 +0100

    Created setCombinedProduction in UpgradeRoom

--- src/UpgradeRoom.java
+++ src/UpgradeRoom.java
@@ -2,4 +2,8 @@ public class UpgradeRoom {
     public UpgradePath upgradePathSpeed;
     public UpgradePath upgradePathQuantity;
     public int combinedProduction;
+
+    public void setCombinedProduction(){
+        combinedProduction = upgradePathSpeed.getCurrentProduction() * upgradePathQuantity.getCurrentProduction();
+    }
 }
