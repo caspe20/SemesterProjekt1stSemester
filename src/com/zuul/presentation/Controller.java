f05ccbf175c930c671c4755cd53302589b8561c6
Macoh20 <macoh20@student.sdu.dk>
2020-11-25 12:14:01 +0100

    Commit to main

--- src/com/zuul/presentation/Controller.java
+++ src/com/zuul/presentation/Controller.java
@@ -13,9 +13,51 @@ public class Controller {
     private Label microplast;
 
     @FXML
-    private TextArea roomdiscription;
-
-
+    private Label plastPerDay;
+    @FXML
+    private Label plastInSea;
+    @FXML
+    private Label fishSouls;
+    @FXML
+    private TextArea roomDescription;
+    @FXML
+    private TextArea userDescription;
+    @FXML
+    private Label roomName;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label upgrade1;
+    @FXML
+    private Label upgrade2;
+    @FXML
+    private Label upgrade1Amount;
+    @FXML
+    private Label upgrade2Amount;
+    @FXML
+    private Button upgradeButton1;
+    @FXML
+    private Label upgrade3;
+    @FXML
+    private Label upgrade4;
+    @FXML
+    private Label upgrade3Amount;
+    @FXML
+    private Label upgrade4Amount;
+    @FXML
+    private Button upgradeButton2;
+    @FXML
+    private Label production;
+    @FXML
+    private Button buttonUp;
+    @FXML
+    private Button buttonDown;
+    @FXML
+    private Button buttonRight;
+    @FXML
+    private Button buttonLeft;
+    @FXML
+    private Label deadFish;
     @FXML
     void updateYear() {
         year.setText(PlayGame.getTimeStat());
