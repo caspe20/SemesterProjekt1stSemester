996e9b1f09370cc2bc765dbac98cae4d90635e4f
Speered <caspertaccopiv@gmail.com>
2020-12-01 09:14:21 +0100

    Forarbejde til GUI og sletelse af unødige elementer

--- src/com/zuul/application/rooms/CarDealer.java
+++
@@ -1,9 +0,0 @@
-package com.zuul.application.rooms;
-
-import com.zuul.application.UpgradePath;
-
-public class CarDealer extends UpgradeRoom{
-    public CarDealer(String description, UpgradePath upgradePathSpeed, UpgradePath upgradePathQuantity) {
-        super(description, upgradePathSpeed, upgradePathQuantity);
-    }
-}
