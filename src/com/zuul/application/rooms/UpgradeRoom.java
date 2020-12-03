0a5aeca5f3b9aee742cfe795de87804064439091
Simon <simok20@student.sdu.dk>
2020-12-03 12:26:54 +0100

    Make room description print to screen

--- src/com/zuul/application/rooms/UpgradeRoom.java
+++ src/com/zuul/application/rooms/UpgradeRoom.java
@@ -11,8 +11,8 @@ public class UpgradeRoom extends Room {
     public double combinedProduction;
 
     // Constructors
-    public UpgradeRoom(String description, UpgradePath upgradePathProducts, UpgradePath upgradePathUsage) {
-        super(description);
+    public UpgradeRoom(String roomName, String description, UpgradePath upgradePathProducts, UpgradePath upgradePathUsage) {
+        super(roomName, description);
         this.upgradePathProducts = upgradePathProducts;
         this.upgradePathUsage = upgradePathUsage;
     }
@@ -29,8 +29,14 @@ public class UpgradeRoom extends Room {
     }
 
     @Override
-    public String getLongDescription() {
-        String s = "Du er " + description + ". Følgende muligheder er til rådighed:\n\n";
+    public String getRoomDescription() {
+        return description;
+
+        /* Jeg tror ikke at vi har brug for en ScreenWriter længere. Vi må sørge for at vise den
+            i implementationen i 1. iteration.
+         */
+
+        /*
         s = s + ScreenWriter.getCenter("Upgrade #1  [" + upgradePathProducts.getPathName() + "]\n");
         s = s + ScreenWriter.getLeftRight(upgradePathProducts.getUpgradeName(0), " --> " + upgradePathProducts.getUpgradeName(1) + "\n");
         s = s + ScreenWriter.getLeftRight(String.valueOf(upgradePathProducts.getUpgradeCoefficient(0)), " --> " + upgradePathProducts.getUpgradeCoefficient(1) + "\n");
@@ -39,6 +45,8 @@ public class UpgradeRoom extends Room {
         s = s + ScreenWriter.getLeftRight(upgradePathUsage.getUpgradeName(0), " --> " + upgradePathUsage.getUpgradeName(1) + "\n");
         s = s + ScreenWriter.getLeftRight(String.valueOf(upgradePathUsage.getUpgradeCoefficient(0))," --> " + upgradePathUsage.getUpgradeCoefficient(1) + "\n");
         s = s + ScreenWriter.getLeftRight("Upgrade price: ", upgradePathUsage.getUpgradePrice(1) + " Fish souls\n");
+
         return s;
+         */
     }
 }
